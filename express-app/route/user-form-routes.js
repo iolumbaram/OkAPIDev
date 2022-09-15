af5899e17c6fc83699bdcbd2e54b8e136ac4e34d
Hye Wook Jung <jung_hye_wook@jtc.gov.sg>
2022-09-16 02:58:43 +0800

    chore: remove comments, unused script - housekeeping

--- express-app/route/user-form-routes.js
+++ express-app/route/user-form-routes.js
@@ -26,62 +26,23 @@ router.route('/submit').post((req, res, next) => {
         });
 
         res.on('end', () => {
-            // console.log('Response ended: ');
-            const telegramresponse = JSON.parse(Buffer.concat(data).toString());
-            // console.log(telegramresponse)
-
-            // for (user of users) {
-            //     console.log(`Got user with id: ${user.id}, name: ${user.name}`);
-            // }
         });
+        
     }).on('error', err => {
         console.log('Error: ', err.message);
     });
 });
 
 router.route('/').get((req, res) => {
-    // user.find((error, data) => {
-    //     if (error) {
-    //         return next(error)
-    //     } else {
-    //         res.json(data)
-    //     }
-    // })
 })
 
 router.route('/edit/:id').get((req, res) => {
-    // user.findById(req.params.id, (error, data) => {
-    //     if (error) {
-    //         return next(error)
-    //     } else {
-    //         res.json(data)
-    //     }
-    // })
 })
 
 router.route('/update/:id').put((req, res, next) => {
-    // user.findByIdAndUpdate(req.params.id, {
-    //     $set: req.body
-    // }, (error, data) => {
-    //     if (error) {
-    //         return next(error);
-    //         console.log(error)
-    //     } else {
-    //         res.json(data)
-    //         console.log('User updated successfully !')
-    //     }
-    // })
 })
+
 router.route('/delete/:id').delete((req, res, next) => {
-    // user.findByIdAndRemove(req.params.id, (error, data) => {
-    //     if (error) {
-    //         return next(error);
-    //     } else {
-    //         res.status(200).json({
-    //             msg: data
-    //         })
-    //     }
-    // })
 })
 
 module.exports = router;
