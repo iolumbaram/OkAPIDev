cf0a17db9872c39aef9acc331978820ddd6a841f
Hye Wook Jung <jung_hye_wook@jtc.gov.sg>
2022-09-16 03:02:47 +0800

    fix: console log build fail

--- express-app/route/user-form-routes.js
+++ express-app/route/user-form-routes.js
@@ -18,8 +18,8 @@ router.route('/submit').post((req, res, next) => {
         let data = [];
         
         const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-        console.log('Status Code:', res.statusCode);
-        console.log('Date in Response header:', headerDate);
+        // console.log('Status Code:', res.statusCode);
+        // console.log('Date in Response header:', headerDate);
 
         res.on('data', chunk => {
             data.push(chunk);
@@ -27,9 +27,9 @@ router.route('/submit').post((req, res, next) => {
 
         res.on('end', () => {
         });
-        
+
     }).on('error', err => {
-        console.log('Error: ', err.message);
+        // console.log('Error: ', err.message);
     });
 });
 
